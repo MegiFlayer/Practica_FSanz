a6e62e6f3d60e0eef2475113e4a2a4fe3d180ec6
Fernando Sanz <fernandosanzeleven@gmail.com>
2018-01-30 00:12:02 +0100

    Add GameLoop y cosas varias

--- JavaApplication2/src/mgf/training/GameLoopClass.java
+++ JavaApplication2/src/mgf/training/GameLoopClass.java
@@ -5,16 +5,36 @@
  */
 package mgf.training;
 
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import kp.jngg.GameLoop;
 /**
  *
  * @author ferna
  */
-public class GameLoopClass {
+public class GameLoopClass implements GameLoop{
     
-    public class GameLoopClass implements GameLoop{
+        private double pX;
+
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void draw(Graphics2D gd) {
+        
+        gd.setColor(Color.CYAN);
+        gd.drawString("Me cago en todo", 100, 10);
+        
+    }
+
+    @Override
+    public void update(double d) {
+        
+    }
         
         
     }
-    }
     
-}
--- JavaApplication2/src/mgf/training/Main.java
+++ JavaApplication2/src/mgf/training/Main.java
@@ -5,10 +5,23 @@
  */
 package mgf.training;
 
+import kp.jngg.Display;
+import kp.jngg.DisplayMode;
+import kp.jngg.GameLoop;
+
 /**
  *
  * @author ferna
  */
 public class Main {
+    
+    public static void main (String [] args){
+    
+        Display ventana = Display.create("Práctica", DisplayMode.getDisplayMode(1280, 720));
+    
+        ventana.setGameLoop(new GameLoopClass());
+        ventana.start();
+        
+    }
     
 }
