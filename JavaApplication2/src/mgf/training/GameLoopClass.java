1d4ba6fa46906b0e1b5751c0d331d0edf4bfe5dd
Fernando Sanz <fernandosanzeleven@gmail.com>
2018-01-29 23:49:52 +0100

    Add librerías Marc y creación pruebas

---
+++ JavaApplication2/src/mgf/training/GameLoopClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mgf.training;
+
+/**
+ *
+ * @author ferna
+ */
+public class GameLoopClass {
+    
+    public class GameLoopClass implements GameLoop{
+        
+        
+    }
+    }
+    
+}
