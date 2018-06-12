7805c1e3b6dee6186a914172a3afddbdae477ef8
Fernando Sanz <fernandosanzeleven@gmail.com>
2018-06-13 00:21:09 +0200

    Add primeros saltos del Mario

--- EjerciciosJava_FSanz/src/mgf/training/Entidad.java
+++ EjerciciosJava_FSanz/src/mgf/training/Entidad.java
@@ -5,6 +5,8 @@
  */
 package mgf.training;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import kp.jngg.geom.Vector2;
 import kp.jngg.input.InputEvent;
@@ -16,101 +18,126 @@ import kp.jngg.sprite.Sprite;
  *
  * @author ferna
  */
-public class Entidad
-{
+public class Entidad {
+
     /**
-     * Funciones recomendadas:
-     * draw: Para dibujar
-     * update: Actualizar valores
+     * Funciones recomendadas: draw: Para dibujar update: Actualizar valores
      * dispatchEvents: capturar eventos de inputs
      */
-    
-    private Vector2 position;
-    private Vector2 size;
-    private Vector2 speed;
+    private static final double X_SPEED = 10;
+    private static final double GRAVITY = 0.1;
+    private static final double FLOOR = 720;
+    private static final double FRICTION = 0.25;
+
+    private final Vector2 position;
+    private final Vector2 size;
+    private final Vector2 speed;
     private Sprite sprite;
     private boolean jumpAble;
-    
+
     private int moveX;
     private int moveY;
-    
-    public Entidad()
-    {
+
+    public Entidad() {
         position = new Vector2();
         size = new Vector2(50, 50);
-        speed = new Vector2(10, 20);
+        speed = new Vector2();
         sprite = null;
     }
-    
-    public void setPosition(double x, double y)
-    {
+
+    public void setPosition(double x, double y) {
         position.x = x;
         position.y = y;
     }
-    
-    public void setSpeed(double x, double y)
-    {
+
+    public void setSpeed(double x, double y) {
         speed.x = x;
         speed.y = y;
     }
-    
-    public Vector2 getSpeed() { return speed.copy(); }
-    
-    public void setSize(double width, double height)
-    {
-        if(width < 1)
+
+    public Vector2 getSpeed() {
+        return speed.copy();
+    }
+
+    public void setSize(double width, double height) {
+        if (width < 1) {
             width = 1;
-        if(height < 1)
+        }
+        if (height < 1) {
             height = 1;
+        }
         size.set(width, height);
     }
-    
-    public void setSprite(Sprite s)
-    {
+
+    public void setSprite(Sprite s) {
         sprite = s;
     }
-    
-    public void draw(Graphics2D g)
-    {
-        if(sprite != null)
+
+    public void draw(Graphics2D g) {
+        if (sprite != null) {
             sprite.draw(g, position.x, position.y, size.x, size.y);
-    }
-    
-    public void update(double delta)
-    {
-        if (jumpAble == true){position.add(speed.x * moveX, speed.y * moveY);}else if (jumpAble == false){position.add(speed.x * moveX, 0);}
-        if (position.y < 620){
-            
-            position.add(0, 7.5);
-        
-        }else if(position.y >= 620){
-        
-            position.y = 620;
-            jumpAble = true;
-        
         }
-        if(position.y <= 420){
-            jumpAble = false;
-            position.y = 420;
-            }
-        
+        drawSpecs(g);
     }
-    
-    public void dispatch(InputEvent event)
-    {
-        if(event.getIdType() == InputId.KEYBOARD_TYPE)
-        {
+
+    private void drawSpecs(Graphics2D g) {
+        Color old = g.getColor();
+        g.setColor(Color.GREEN);
+        g.drawString("Position = " + position, 12, 12);
+        g.drawString("Speed = " + speed, 12, 24);
+        g.setColor(old);
+    }
+
+    public void update(double delta) {
+
+        speed.y += GRAVITY;
+
+        if (jumpAble && moveY < 0) {
+            speed.y = -5;
+            jumpAble = false;
+        }
+        if (jumpAble && moveX != 0) {
+            speed.x = X_SPEED * moveX;
+        } else {
+            if (speed.x > 0) {
+                speed.x -= FRICTION * (jumpAble ? 1 : 0.25);
+                if (speed.x < 0) {
+                    speed.x = 0;
+                }
+            } else if (speed.x < 0) {
+                speed.x += FRICTION * (jumpAble ? 1 : 0.25);
+                if (speed.x > 0) {
+                    speed.x = 0;
+                }
+            }
+        }
+        speed.ensureRangeLocal(10, 10);
+
+        position.add(speed);
+
+        if (position.y + size.y >= FLOOR) {
+            position.y = FLOOR - size.y;
+            jumpAble = true;
+        } else {
+            jumpAble = false;
+        }
+
+    }
+
+    public void dispatch(InputEvent event) {
+        if (event.getIdType() == InputId.KEYBOARD_TYPE) {
             int code = event.getCode();
-            if(code == Keycode.VK_UP && jumpAble == true) {
+            if (code == Keycode.VK_UP) {
                 moveY += event.isPressed() ? -1 : 1;
             }
-            if(code == Keycode.VK_DOWN && position.y <= 620) {
+
+            if (code == Keycode.VK_DOWN && position.y <= 620) {
                 moveY += event.isPressed() ? 1 : -1;
             }
-            if(code == Keycode.VK_LEFT) {
+            if (code == Keycode.VK_LEFT) {
                 moveX += event.isPressed() ? -1 : 1;
             }
-            if(code == Keycode.VK_RIGHT) {
+            if (code == Keycode.VK_RIGHT) {
                 moveX += event.isPressed() ? 1 : -1;
             }
         }
